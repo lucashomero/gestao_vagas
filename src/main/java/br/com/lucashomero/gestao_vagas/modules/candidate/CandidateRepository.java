3f4d1c5813e9358e5015262066c8731dc669a399
lucashomero <lucashomerovs@gmail.com>
2025-06-25 10:01:51 -0300

    code: criando db usando docker

---
+++ src/main/java/br/com/lucashomero/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,12 @@
+package br.com.lucashomero.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
+
+	
+}
+
+
